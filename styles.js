3612da810eacf9ac9fe6206efb116f76c536bd04
TaylorTech369 <joaovictormendonca0411@gmail.com>
2025-11-03 15:56:42 -0300

    Add styles for container and button components

---
+++ styles.js
@@ -0,0 +1,47 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  botaozinho: {
+    backgroundColor: "#F40B5D",
+    borderRadius: 30,
+    padding: 10, 
+    width: 350,
+    margin: 5,
+  },
+
+  textodobotaozinho: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 17,
+    fontStyle: "bold",
+  },
+
+  tituloLogin: {
+    marginTop: 250,
+    fontSize: 55,
+  },
+
+  hrs: {
+    width: 100,
+  },
+
+  input1: {
+    borderColor: "#F40B5D",
+    borderWidth: 1,
+    borderRadius: 30,
+    padding: 10, 
+    width: 350,
+    margin: 5,
+  },
+
+  inputs: {
+    padding: 10,
+    borderColor: "black",
+  },
+
+});
