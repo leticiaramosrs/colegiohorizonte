2a0c610cd4d11ae05fff6b17d9df094e7ba00c53
leticiaramosrs <leleramosvieira@gmail.com>
2025-11-03 17:11:41 -0300

    login

--- App.js
+++ App.js
@@ -1,71 +1,25 @@
-
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { TextInput } from 'react-native-web';
+import { Text, TouchableOpacity, View, TextInput, Image } from 'react-native';
+import styles from './styles'; 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.tudinho}>
-      <Text style={styles.tituloLogin}>Login</Text>
-      <hr style={styles.hrs}></hr>
-      
-      <View>
 
-      <View style={styles.inputs}>
+      <View style={styles.tudo}>
+        <Image style={styles.logo} source={ require('./assets/logoColegiocopia1.png')}></Image>
+        <Text style={styles.textodecima}>Entre na sua conta de estudante </Text>
+        <View style={styles.inputs}>
+          <Text style={styles.texto}>Nome completo</Text>
+          <TextInput style={styles.input1} placeholder="Digite seu nome completo" />
 
-      <Text style={styles.texto}>Matricula</Text>
-      <TextInput style={styles.input1} placeholder='Digite sua Matricula'></TextInput>
+          <Text style={styles.texto}>Matrícula</Text>
+          <TextInput style={styles.input1} placeholder="Digite sua matrícula" />
+        </View>
 
-      <Text style={styles.texto}>Senha</Text>
-      <TextInput style={styles.input1 } placeholder='Digite sua senha'></TextInput>
+        <TouchableOpacity style={styles.botaozinho}>
+          <Text style={styles.textodobotaozinho}>Entrar</Text>
+        </TouchableOpacity>
+
       </View>
 
-      </View>
-    </View>
-    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: "gray",
-    alignItems: 'center',
-    // justifyContent: 'center',
-    // height: 500
-  },
-
-  tudinho: {
-    // backgroundColor: "gray",
-    alignItems: 'center',
-    justifyContent: 'center',
-    // borderWidth: 2,
-    // borderRadius: 30,
-    height: 75,
-    padding: 10, 
-    marginTop: 250,
-  },
-
-  tituloLogin: {
-    fontSize: 55,
-  },
-
-  hrs: {
-    width: 50,
-  },
-
-  input1: {
-    // borderColor: "cyan",
-    // color: "cyan",
-    borderWidth: 1,
-    borderRadius: 30,
-    padding: 10, 
-    width: 350,
-    margin: 5,
-  },
-
-  inputs: {
-    padding: 10,
-  },
-
-});
---
+++ styles.js
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  tudo: {
+    backgroundColor: "white",
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  botaozinho: {
+    backgroundColor: "#F40B5D",
+    borderRadius: 30,
+    padding: 10, 
+    width: 350,
+    margin: 5,
+
+  },
+
+
+  textodobotaozinho: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 17,
+  },
+
+  input1: {
+    backgroundColor: "#FFF2F9",
+    borderColor: "#F40B5D",
+    borderWidth: 1,
+    borderRadius: 30,
+    padding: 10, 
+    width: 330,
+    margin: 5,
+    color: 'gray',
+  },
+
+  texto: {
+    
+  },
+
+  inputs: {
+    padding: 10,
+    borderColor: "black",
+  },
+
+  logo: {
+    marginTop: 170,
+    width: 300,
+    height: 300,
+    padding: 20,
+  },
+  textodecima: {
+    fontSize: 40,
+  }
+});
