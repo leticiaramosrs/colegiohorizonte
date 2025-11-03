d47dbbea87452065530cb190969223cf33ff2770
LETÍCIA VIEIRA RAMOS <leticia_v_ramos@estudante.sesisenai.org.br>
2025-11-03 14:34:14 -0300

    commit teste

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tudinho}>
+      <Text style={styles.tituloLogin}>Login</Text>
+      <hr style={styles.hrs}></hr>
+      
+      <View>
+
+      <View style={styles.inputs}>
+
+      <Text style={styles.texto}>Matricula</Text>
+      <TextInput style={styles.input1} placeholder='Digite sua Matricula'></TextInput>
+
+      <Text style={styles.texto}>Senha</Text>
+      <TextInput style={styles.input1 } placeholder='Digite sua senha'></TextInput>
+      </View>
+
+      </View>
+    </View>
     </View>
   );
 }
@@ -13,8 +29,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: "gray",
+    alignItems: 'center',
+    // justifyContent: 'center',
+    // height: 500
+  },
+
+  tudinho: {
+    // backgroundColor: "gray",
     alignItems: 'center',
     justifyContent: 'center',
+    // borderWidth: 2,
+    // borderRadius: 30,
+    height: 75,
+    padding: 10, 
+    marginTop: 250,
   },
+
+  tituloLogin: {
+    fontSize: 55,
+  },
+
+  hrs: {
+    width: 50,
+  },
+
+  input1: {
+    // borderColor: "cyan",
+    // color: "cyan",
+    borderWidth: 1,
+    borderRadius: 30,
+    padding: 10, 
+    width: 350,
+    margin: 5,
+  },
+
+  inputs: {
+    padding: 10,
+  },
+
 });
